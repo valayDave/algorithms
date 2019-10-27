bbf73ac05e3fbd1983262d8250ab302b79bde375
Valay Dave <valaygaurang@gmail.com>
2019-10-26 21:34:36 -0700

    43/66 Tests.

--- maximal_rectangle.py
+++ maximal_rectangle.py
@@ -30,23 +30,28 @@ def maximal_rectangle(matrix: List[List[str]]):
             one_arr.append((start_index,len(row)-1))
         one_indexes.append(one_arr)
     max_counter = 0
-    print(one_indexes)
+    # print(one_indexes)
     for i in range(len(one_indexes)):
         row_index_tuple_arr = one_indexes[i]
         for start_index,end_index in row_index_tuple_arr:
             current_units_counter = (end_index+1)-start_index
             current_units = current_units_counter 
-            print("Setting Current Units Of ",current_units,start_index,end_index) 
-            for j in range(i+1,len(one_indexes)):
+            # print("Setting Current Units Of ",current_units,start_index,end_index)
+            j_start = i-1 if i > 0 else i+1  
+            for j in range(j_start,len(one_indexes)):
+                if j == i:
+                    continue
                 next_row_index_tuple_arr = one_indexes[j]
                 for next_start_index,next_end_index in next_row_index_tuple_arr:
-                    print((start_index,end_index),i,'      ',(next_start_index,next_end_index),current_units,j)
-                    if (start_index == next_start_index and end_index == start_index) or (start_index <= next_end_index and next_start_index <= start_index and (next_end_index+1 - next_start_index)>=current_units_counter):
-                        pass # $ TODO : Calculate Coverage. 
+                    if (start_index == next_start_index and end_index == start_index):
                         current_units += current_units_counter
+                    elif ((start_index <= next_end_index) and next_start_index <= start_index and (next_end_index+1 - next_start_index)>=current_units_counter) and next_end_index >=end_index:
+                        current_units += current_units_counter
+                        # print((start_index,end_index),i,'      ',(next_start_index,next_end_index),current_units,j)
                     # else:
                     #     break
             if current_units > max_counter:
+                # print("setting Counter :::: ",current_units)
                 max_counter = current_units
     return max_counter
 
@@ -85,7 +90,16 @@ k = [
     ["1","1","1","1","1","0","0","0"],
     ["0","1","1","1","1","0","0","0"]
 ]
-# print(maximal_rectangle(x))
-# print(maximal_rectangle(y))
-# print(maximal_rectangle(t))
-print(maximal_rectangle(k))
+q = [
+    ["0","1","1","0","1"],
+    ["1","1","0","1","0"],
+    ["0","1","1","1","0"],
+    ["1","1","1","1","0"],
+    ["1","1","1","1","1"],
+    ["0","0","0","0","0"]
+]
+print(maximal_rectangle(x))
+print(maximal_rectangle(y))
+print(maximal_rectangle(t))
+print(maximal_rectangle(z))
+print(maximal_rectangle(q))
